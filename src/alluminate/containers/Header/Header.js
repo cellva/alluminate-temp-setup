aae1c0965f684d25e3a446d201308943bf68fee2
vivi28 <vivekanandan@ideas2it.com>
2017-10-09 13:40:53 +0530

    Fix: Header allignment fix

--- src/alluminate/containers/Header/Header.js
+++ src/alluminate/containers/Header/Header.js
@@ -1,72 +1,64 @@
 import React, { Component } from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-
+import {
+  Nav
+} from 'react-bootstrap/lib';
 const style = require('./Header.scss');
 
 export default class Header extends Component {
   render() {
     return (
-      <div className={style.headerContainer}>
-        <a href="#">
-          <img className={style.logo + ' hidden-xs'} src="logo.png" width="150" alt=""/>
-          <img className={style.logoXs + ' visible-xs'} src="logo.png" width="150" alt=""/>
-        </a>
-        <VerticalDivider/>
-        <div className="hidden-xs">
-          <LinkContainer to="/portfolio">
-            <div className={style.link}>My Portfolio</div>
-          </LinkContainer>
-        </div>
-        <VerticalDivider/>
-        <CustomMenu/>
-        <VerticalDivider/>
-        <CustomSearch className={style.link}/>
-        <div className={style.userProfile}>
-          <VerticalDivider/>
-          <a href="#">
-            <img src="default-user.png" width="40" alt="" />
-            <span className="hidden-xs">John</span>
-          </a>
-          {/* Show the portfolio link and user name in the user profile menu for xs device. */}
-          {/* <LinkContainer className="visible-xs" to="/portfolio">
-            <div className={style.link}>My Portfolio</div>
-          </LinkContainer> */}
-        </div>
-      </div>
-    );
-  }
-}
-
-class VerticalDivider extends Component {
-  render() {
-    return (
-      <div className={style.verticalDivider}></div>
-    );
-  }
-}
-
-class CustomMenu extends Component {
-  render() {
-    return (
-      <div className={style.link}>
-        Sector <i className={style.arrow + ' fa fa-angle-down'} aria-hidden="true"></i>
-        {/* <CustomMenu/> */}
-      </div>
-    );
-  }
-}
-
-class CustomSearch extends Component {
-  render() {
-    return (
-      <div className={style.search}>
-        <form className="form-inline">
-          <input type="text" placeholder="Search" />
-          <button className={style.btn + ' btn hidden-xs hidden-sm'} type="submit">
-            <i className="fa fa-search" aria-hidden="true"></i>
+      <Nav className={`${style.customNav} navbar navbar-expand-md fixed-top`}>
+        <div className="navbar-header">
+          <button type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
-        </form>
-      </div>
+          <a className={style.navbarBrand} href="#">
+            <img className={style.logo + ' hidden-xs'} src="logo.png" width="150" alt="Logo"/>
+            <img className={style.logoXs + ' visible-xs'} src="logo.png" width="150" alt="Logo"/>
+          </a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className={`${style.myNavbar} navbar-nav`}>
+            <li className="nav-item">
+              <a href="#">My Portfolio</a>
+            </li>
+            <li className={`${style.custoNavItem} nav-item dropdown`}>
+              <a href="#" id="dropdown01"
+                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 Sectors <i className="fa fa-angle-down" aria-hidden="true" />
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                <a className="dropdown-item" href="#">currency</a>
+                <a className="dropdown-item" href="#">currency</a>
+                <a className="dropdown-item" href="#">currency</a>
+              </div>
+            </li>
+            <li className={`${style.searchHeader} nav-item`}>
+              <form className="form-inline my-2 my-lg-0">
+                <input type="text" placeholder="Search for an ICO" aria-label="Search" />
+                <button className="btn my-2 my-sm-0" type="submit">
+                  <i className="fa fa-search" aria-hidden="true" />
+                </button>
+              </form>
+            </li>
+          </ul>
+          <div className={style.userLogout}>
+            <a href="#">
+              <img src="default-user.png" width="40" alt="" />
+              <span>John</span>
+            </a>
+          </div>
+        </div>
+      </Nav>
     );
   }
 }
